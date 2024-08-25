6b0a8514fe3d0e6f682864eec0d3a5bc57734982
Muxsinjon1610 <muxsinjon1610@gmail.com>
2024-08-26 00:02:52 +0500

    finished

--- src/pages/modals/components/modal.tsx
+++ src/pages/modals/components/modal.tsx
@@ -1,7 +1,26 @@
-export const Modal = () => {
+interface modalTypes {
+  title?: string;
+  desription: string;
+  onClose: () => void;
+}
+
+export const Modal = ({ title, desription, onClose }: modalTypes) => {
   return (
     <>
-      <div className="absolute inset-0"></div>
+      <div onClick={onClose} className="absolute inset-0 opacity-90 z-50 bg-gray-400">
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className="bg-white max-w-[500px] py-5 px-9 rounded-20 mx-auto mt-[200px]"
+        >
+          <div className="flex items-center justify-between">
+            <h3 className="text-xl font-medium mb-4">{title}</h3>
+            <p onClick={onClose} className="text-xl cursor-pointer font-bold">
+              x
+            </p>
+          </div>
+          <p className="text-lg">{desription}</p>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/modals/modals.tsx
+++ src/pages/modals/modals.tsx
@@ -1,5 +1,29 @@
+import React from "react";
+import Modal from "./components/modal";
+import Button from "../buttons/components/button";
+
 export const Modals = () => {
-  return <></>;
+  const [modal, setModal] = React.useState(false);
+  return (
+    <>
+      <div>
+        <Button className="w-full"
+          onClick={() => setModal(!modal)}
+          variant="primary"
+          children="Open modal"
+        />
+        {modal && (
+          <Modal
+            onClose={() => setModal(!modal)}
+            title="Title of modal"
+            desription=" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ab
+        aliquid pariatur qui dicta delectus ea nesciunt doloribus culpa. Quam
+        optio neque quisquam architecto earum facere ipsum aut impedit omnis!"
+          />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Modals;
